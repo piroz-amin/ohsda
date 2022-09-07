bd6a5505a81981b269a54673e67f4840da39466c
piroz-amin <piroz.amin99@gmail.com>
2022-09-07 18:13:34 +0430

    Updates

---
+++ precache-manifest.ff2135495e2dfff9d83d3a925a16a26d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b844bc7dce2b64d4db181d05c094106b",
+    "url": "/piroz-amin/ohsda.git/index.html"
+  },
+  {
+    "revision": "3bbf4ce23e294364ebd8",
+    "url": "/piroz-amin/ohsda.git/static/css/2.709e72d0.chunk.css"
+  },
+  {
+    "revision": "dc1d71bdcc6d18f4d05c",
+    "url": "/piroz-amin/ohsda.git/static/css/main.b722454f.chunk.css"
+  },
+  {
+    "revision": "3bbf4ce23e294364ebd8",
+    "url": "/piroz-amin/ohsda.git/static/js/2.6d887c06.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/piroz-amin/ohsda.git/static/js/2.6d887c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc1d71bdcc6d18f4d05c",
+    "url": "/piroz-amin/ohsda.git/static/js/main.41e7df69.chunk.js"
+  },
+  {
+    "revision": "5fde99ecd1ee3e9ef329",
+    "url": "/piroz-amin/ohsda.git/static/js/runtime-main.9d0f8724.js"
+  },
+  {
+    "revision": "80bb4d09b51c299392a611a0673a89bf",
+    "url": "/piroz-amin/ohsda.git/static/media/Logo.80bb4d09.png"
+  },
+  {
+    "revision": "5939ff1c213b5148093ce04a26c6191e",
+    "url": "/piroz-amin/ohsda.git/static/media/Moto.5939ff1c.svg"
+  },
+  {
+    "revision": "1b604e530428c93b0c62f99a76590455",
+    "url": "/piroz-amin/ohsda.git/static/media/Oldman.1b604e53.webp"
+  },
+  {
+    "revision": "61bfebf0f80205218a97bf54188cfbf5",
+    "url": "/piroz-amin/ohsda.git/static/media/afghanKid.61bfebf0.jpg"
+  },
+  {
+    "revision": "70db76e82f60cb203c65a0eee9013b64",
+    "url": "/piroz-amin/ohsda.git/static/media/fightforfood.70db76e8.jpeg"
+  },
+  {
+    "revision": "5701f15903767c13f3e145f669151319",
+    "url": "/piroz-amin/ohsda.git/static/media/girl.5701f159.jpg"
+  },
+  {
+    "revision": "d6311da6f5940119587078b572e7aa74",
+    "url": "/piroz-amin/ohsda.git/static/media/refugee.d6311da6.webp"
+  },
+  {
+    "revision": "3b866c4c89be14f66302f425dfce2360",
+    "url": "/piroz-amin/ohsda.git/static/media/service1.3b866c4c.jfif"
+  },
+  {
+    "revision": "13a99e0f1161a1b9df7d64f65cc7f75b",
+    "url": "/piroz-amin/ohsda.git/static/media/service2.13a99e0f.jfif"
+  },
+  {
+    "revision": "ddf892168e3333e9bb57e13d28088d66",
+    "url": "/piroz-amin/ohsda.git/static/media/service3.ddf89216.jfif"
+  }
+]);
